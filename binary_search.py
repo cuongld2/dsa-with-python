bd7be694ba4741ce640e5003839e52814219a4fd
Donald Le <ledinhcuong99@gmail.com>
2022-11-28 10:24:33 +0700

    Fix format

--- binary_search.py
+++ binary_search.py
@@ -1,18 +1,20 @@
 
 
-def binary_search(my_list,search_item):
+def binary_search(my_list, search_item):
     low = 0
-    high=len(my_list)-1
-    while low<=high:
+    high = len(my_list)-1
+    while low <= high:
         mid = (low+high)//2
-        if search_item==my_list[mid]:
+        if search_item == my_list[mid]:
             return mid
-        elif search_item>my_list[mid]:
-            low=mid+1
-        elif search_item<my_list[mid]:
-            high=mid-1
+        elif search_item > my_list[mid]:
+            low = mid + 1
+        elif search_item < my_list[mid]:
+            high = mid - 1
     return "Not_found"
 
-list_items=[1,3,4,6,7,15,99,100,100,106,108,200,260]
 
-print(binary_search(list_items,100))
+if __name__ == "__main__":
+    list_items = [1, 3, 4, 6, 7, 15, 99, 100, 100, 106, 108, 200, 260]
+
+    print(binary_search(list_items, 100))
--- sort.py
+++ sort.py
@@ -1,33 +1,28 @@
-
 # 1. Bubble Sort
-
-import mailcap
-from operator import le
-
 
 def bubble_sort(my_list):
     n = len(my_list)
     for i in range(n):
-        for j in range(0,n-i-1):
-            if my_list[j]>my_list[j+1]:
-                my_list[j],my_list[j+1]=my_list[j+1],my_list[j]
+        for j in range(0, n - i - 1):
+            if my_list[j] > my_list[j + 1]:
+                my_list[j], my_list[j + 1] = my_list[j + 1], my_list[j]
     return my_list
 
 
 # 2. Selection sort
 
 def find_smallest_number(my_list):
-    n=len(my_list)
-    smallest_index=0
-    for i in range(1,n):
-        if my_list[smallest_index]>my_list[i]:
+    n = len(my_list)
+    smallest_index = 0
+    for i in range(1, n):
+        if my_list[smallest_index] > my_list[i]:
             smallest_index = i
     return smallest_index
 
 
 def selection_sort(my_list):
-    n= len(my_list)
-    sorted_list=[]
+    n = len(my_list)
+    sorted_list = []
     for i in range(n):
         temp_smallest = find_smallest_number(my_list)
         sorted_list.append(my_list.pop(temp_smallest))
@@ -37,7 +32,7 @@ def selection_sort(my_list):
 # 3. merge sort
 
 def merge_sort(my_list):
-    n=len(my_list)
+    n = len(my_list)
     if n > 1:
         mid = n // 2
         left = my_list[:mid]
@@ -45,58 +40,59 @@ def merge_sort(my_list):
         merge_sort(left)
         merge_sort(right)
 
-
         i = 0
         j = 0
         k = 0
-        while i<len(left) and j<len(right):
-            if left[i]<right[j]:
-                my_list[k]=left[i]
-                i+=1
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                my_list[k] = left[i]
+                i += 1
             else:
-                my_list[k]=right[j]
-                j+=1
-            k+=1
-        while i<len(left):
-            my_list[k]=left[i]
-            i+=1
-            k+=1
-        
-        while j<len(right):
-            my_list[k]=right[j]
-            j+=1
-            k+=1
+                my_list[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            my_list[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            my_list[k] = right[j]
+            j += 1
+            k += 1
 
     return my_list
 
 
 # 4.Quick sort
 
-def partition(my_list,left,right):
-    i=left
-    j=right-1
-    pivot=my_list[right]
+def partition(my_list, left, right):
+    i = left
+    j = right - 1
+    pivot = my_list[right]
 
     while i < j:
         while i < right and my_list[i] < pivot:
-            i+=1
-        while j > left and my_list[j]>pivot:
-            j-=1
-        if i<j:
-            my_list[i],my_list[j]=my_list[j],my_list[i]
-    
-    if my_list[i]>pivot:
-        my_list[i],my_list[right]=my_list[right],my_list[i]
+            i += 1
+        while j > left and my_list[j] > pivot:
+            j -= 1
+        if i < j:
+            my_list[i], my_list[j] = my_list[j], my_list[i]
+
+    if my_list[i] > pivot:
+        my_list[i], my_list[right] = my_list[right], my_list[i]
     return i
 
 
-def quicksort(my_list,left, right):
-    if left<right:
-        index=partition(my_list,left,right)
-        quicksort(my_list,left, index-1)
-        quicksort(my_list,index+1,right)
+def quicksort(my_list, left, right):
+    if left < right:
+        index = partition(my_list, left, right)
+        quicksort(my_list, left, index - 1)
+        quicksort(my_list, index + 1, right)
 
-the_list = [0,3,2,-4,10,12,-9,-4,1]
 
-quicksort(the_list,0,len(the_list)-1)
-print(the_list)
+if __name__ == "__main__":
+    the_list = [0, 3, 2, -4, 10, 12, -9, -4, 1]
+
+    quicksort(the_list, 0, len(the_list) - 1)
+    print(the_list)
